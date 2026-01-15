33cadab9fc1bbe0d2bdf7085791f1bd2a778f5d0
bruce <bmpandrada@gmail.com>
2026-01-15 13:46:33 +0800

    feat: create layout fictional university custom wordpress

---
+++ functions.php
@@ -0,0 +1,21 @@
+<?php
+
+function fictional_university_files()
+{
+  wp_enqueue_script('fictional_university_main_js', get_theme_file_uri('/build/index.js'), array('jquery'), '1.0', true);
+  wp_enqueue_style('fictional_university_font_awesome', '//maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css');
+  wp_enqueue_style('fictional_university_google_fonts', 'fonts.googleapis.com/css?family=Roboto+Condensed:300,300i,400,400i,700,700i|Roboto:100,300,400,400i,700,700i');
+
+  wp_enqueue_style('fictional_university_main_styles', get_theme_file_uri('/build/style-index.css'));
+  wp_enqueue_style('fictional_university_styles', get_theme_file_uri('/build/index.css'));
+}
+
+add_action('wp_enqueue_scripts', 'fictional_university_files');
+
+function fictional_university_features()
+{
+  add_theme_support('title-tag');
+  // register_nav_menu('headerMenuLocation', 'Header Menu Location');
+}
+
+add_action('after_setup_theme', 'fictional_university_features');

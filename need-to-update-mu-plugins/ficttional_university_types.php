5513d2f8e8bee1f97da49791df5f4f7867eef593
Bruce Andrada <bmpandrada@gmail.com>
2026-01-17 13:16:55 +0000

    feat: add related events and programs

--- need-to-update-mu-plugins/ficttional_university_types.php
+++ need-to-update-mu-plugins/ficttional_university_types.php
@@ -2,6 +2,7 @@
 
 //================= Custom Post Types =================//
 function fictional_university_custom_post_types()
+// Event Post Type
 {
   register_post_type('event', array(
     'public' => true,
@@ -17,6 +18,23 @@ function fictional_university_custom_post_types()
     'supports' => array('title', 'editor', 'excerpt', 'custom-fields'),
     'has_archive' => true,
     'rewrite' => array('slug' => 'events')
+  ));
+
+  //Program Post Type
+  register_post_type('program', array(
+    'public' => true,
+    'show_in_rest' => true,
+    'labels' => array(
+      'name' => 'Programs',
+      'add_new_item' => 'Add New Program',
+      'edit_item' => 'Edit Program',
+      'all_items' => 'All Programs',
+      'singular_name' => 'Program'
+    ),
+    'menu_icon' => 'dashicons-welcome-learn-more',
+    'supports' => array('title', 'editor'),
+    'has_archive' => true,
+    'rewrite' => array('slug' => 'programs')
   ));
 }
 
--- single-program.php
+++ single-program.php
@@ -24,6 +24,63 @@ while (have_posts()) {
     <div class="generic-content">
       <?php the_content(); ?>
     </div>
+
+    <?php
+    $today = date('Ymd');
+    $args = array(
+      'posts_per_page' => 2,
+      'post_type' => 'event',
+      'meta_key' => 'event_date',
+      'orderby' => 'meta_value_num',
+      'order' => 'ASC',
+
+      'meta_query' => array(
+        array(
+          'key' => 'event_date',
+          'compare' => '>=',
+          'value' => $today,
+          'type' => 'NUMERIC'
+        ),
+        array(
+          'key' => 'related_programs',
+          'compare' => 'LIKE',
+          'value' => '"' . get_the_ID() . '"',
+
+        )
+      )
+    );
+    $programsEvents = new WP_Query($args);
+
+    if ($programsEvents->have_posts()) {
+      echo '<hr class="section-break">';
+      echo '<h2 class="headline headline--medium">Upcoming ' . get_the_title() . ' Events</h2>';
+
+      while ($programsEvents->have_posts()) {
+        $programsEvents->the_post(); ?>
+
+        <div class="event-summary">
+          <a class="event-summary__date t-center" href="<?php the_permalink(); ?>">
+            <span class="event-summary__month">
+              <?php
+              $eventDate = new DateTime(get_field('event_date'));
+              echo esc_html($eventDate->format('M')) ?></span>
+            <span class="event-summary__day"><?php echo esc_html($eventDate->format('d')) ?></span>
+          </a>
+          <div class="event-summary__content">
+            <h5 class="event-summary__title headline headline--tiny"><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h5>
+            <p><?php if (has_excerpt()) {
+                  echo get_the_excerpt();
+                } else {
+                  echo wp_trim_words(get_the_content(), 13);
+                } ?><a href="<?php the_permalink(); ?>" class="nu gray">Learn more</a></p>
+          </div>
+        </div>
+    <?php
+      }
+    }
+    ?>
+
+
   </div>
 
 <?php }

7944d1ad59f6e0b050e480dc93c07b7b8ad8eb3f
bruce <bmpandrada@gmail.com>
2026-01-17 21:13:29 +0800

    fix: add navlink active

--- single-program.php
+++ single-program.php
@@ -49,34 +49,35 @@ while (have_posts()) {
         )
       )
     );
-
     $programsEvents = new WP_Query($args);
 
-    while ($programsEvents->have_posts()) {
-      $programsEvents->the_post(); ?>
+    if ($programsEvents->have_posts()) {
+      echo '<hr class="section-break">';
+      echo '<h2 class="headline headline--medium">Upcoming ' . get_the_title() . ' Events</h2>';
 
-      <div class="event-summary">
-        <a class="event-summary__date t-center" href="<?php the_permalink(); ?>">
-          <span class="event-summary__month">
-            <?php
-            $eventDate = new DateTime(get_field('event_date'));
-            echo esc_html($eventDate->format('M')) ?></span>
-          <span class="event-summary__day"><?php echo esc_html($eventDate->format('d')) ?></span>
-        </a>
-        <div class="event-summary__content">
-          <h5 class="event-summary__title headline headline--tiny"><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h5>
-          <p><?php if (has_excerpt()) {
-                echo get_the_excerpt();
-              } else {
-                echo wp_trim_words(get_the_content(), 13);
-              } ?><a href="<?php the_permalink(); ?>" class="nu gray">Learn more</a></p>
+      while ($programsEvents->have_posts()) {
+        $programsEvents->the_post(); ?>
+
+        <div class="event-summary">
+          <a class="event-summary__date t-center" href="<?php the_permalink(); ?>">
+            <span class="event-summary__month">
+              <?php
+              $eventDate = new DateTime(get_field('event_date'));
+              echo esc_html($eventDate->format('M')) ?></span>
+            <span class="event-summary__day"><?php echo esc_html($eventDate->format('d')) ?></span>
+          </a>
+          <div class="event-summary__content">
+            <h5 class="event-summary__title headline headline--tiny"><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h5>
+            <p><?php if (has_excerpt()) {
+                  echo get_the_excerpt();
+                } else {
+                  echo wp_trim_words(get_the_content(), 13);
+                } ?><a href="<?php the_permalink(); ?>" class="nu gray">Learn more</a></p>
+          </div>
         </div>
-      </div>
     <?php
-
+      }
     }
-
-
     ?>
 
 
